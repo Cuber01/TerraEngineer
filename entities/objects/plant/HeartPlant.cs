69d3c31ef7a87abb75474b0c6f469250c954c111
Cuber01 <johnmallard631@gmail.com>
2025-10-20 11:18:00 +0200

    Made heart plant regenerate fruit

--- entities/objects/plant/HeartPlant.cs
+++ entities/objects/plant/HeartPlant.cs
@@ -6,12 +6,42 @@ using TerraEngineer.entities;
 public partial class HeartPlant : Terraformable
 {
     [Export] private int healthHealed = 3;
+    [Export] private float secondsTilRegrow = 5;
     [Export] private AnimatedSprite2D sprite;
-    
+
+    private bool hasFruit = true;
+    private bool timerScheduled = false;
+
+    public override void Enable()
+    {
+        base.Enable();
+        if (!timerScheduled && !hasFruit)
+        {
+            TimerManager.Schedule(secondsTilRegrow, (_) => regrow());
+        }
+    }
+
     private void onBodyEntered(Node2D body)
     {
-        Player player = (Player)body;
-        sprite.Animation = "no_fruit";
-        player.CM.GetComponent<Health>().ChangeHealth(healthHealed);
+        if (hasFruit)
+        {
+            Player player = (Player)body;
+            sprite.Animation = "no_fruit";
+            player.CM.GetComponent<Health>().ChangeHealth(healthHealed);
+            hasFruit = false;    
+            TimerManager.Schedule(secondsTilRegrow, (_) => regrow());
+            timerScheduled = true;
+        }
+    }
+
+    private void regrow()
+    {
+        if (Active)
+        {
+            hasFruit = true;
+            sprite.Animation = "default";    
+        }
+
+        timerScheduled = false;
     }
 }
