f6a53acad48da577953962dc826f492779001ab8
Cuber01 <johnmallard631@gmail.com>
2026-05-07 19:03:00 +0200

    Added turbine

--- entities/objects/turbine/Turbine.cs
+++ entities/objects/turbine/Turbine.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using TerraEngineer.entities.mobs;
 
 namespace TerraEngineer.entities.objects;
@@ -7,8 +8,10 @@ namespace TerraEngineer.entities.objects;
 public partial class Turbine : Entity
 {
 	[Export] private Area2D areaOfEffect;
-	[Export] private PackedScene particleScene;
-	private const float UpwardForce = 100f;
+	private List<Entity> affectedEntities = new List<Entity>();
+	private const float UpwardForce = 1f;
+	private const float maxVelocity = -100f;
+	
 	private GpuParticles2D particles;
 	
 	public override void _Ready()
@@ -20,13 +23,28 @@ public partial class Turbine : Entity
 			return;
 		#endif
 	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		foreach (Entity e in affectedEntities)
+		{
+			e.velocity = new Vector2(e.velocity.X, Math.Min(e.velocity.Y - UpwardForce, maxVelocity));
+		}
+ 	}
 	
 	private void onAreaBodyEntered(Node2D body)
 	{
 		if (body is Entity entity)
 		{
-			entity.velocity = entity.velocity with { Y = -UpwardForce };
+			affectedEntities.Add(entity);
 		}
 	}
+
+	private void onAreaBodyExited(Node2D body)
+	{
+		if (affectedEntities.Contains(body as Entity))
+			affectedEntities.Remove(body as Entity);
+	}
+	
 }
 
