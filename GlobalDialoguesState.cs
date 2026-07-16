cbad375a0528174f15fa6668ab24cb0adb8bcde7
Cuber01 <johnmallard631@gmail.com>
2026-07-16 19:05:04 +0200

    Added remaining files

---
+++ GlobalDialoguesState.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class GlobalDialoguesState : Node
+{
+    // 1. A static pointer to this specific instance
+    public static GlobalDialoguesState Instance { get; private set; }
+
+    // 2. The variable itself MUST be a public, non-static property
+    [Export]
+    public int PuzzleTerraformingRoom_Biome { get; set; } = 0;
+
+    public override void _EnterTree()
+    {
+        // 3. Assign the static instance pointer when the node loads
+        Instance = this;
+    }
+}
